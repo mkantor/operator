b37551373b5c8f225945ce5e1738291ca1623a86
Matt Kantor <the.matt.kantor@gmail.com>
2020-06-03 09:39:05 -0700

    Some basic scaffolding for CLI.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+use std::fmt;
+
+const USAGE: &'static str = "Usage: gluon
+
+Renders a handlebars template from STDIN (eventually).
+
+Try: echo \"hello world\" | gluon";
+
+type Success = ();
+
+enum Error {
+    Unknown(),
+}
+impl fmt::Debug for Error {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let error_message = match self {
+            Error::Unknown() => "Unknown error.",
+        };
+        write!(f, "{}\n\n{}", error_message, USAGE)
+    }
+}
+
+fn main() -> Result<Success, Error> {
+    Err(Error::Unknown())
 }
