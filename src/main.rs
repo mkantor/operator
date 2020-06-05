2e4d41b3cc161dea19256164f8f07a557fa80d16
Matt Kantor <the.matt.kantor@gmail.com>
2020-06-05 09:10:33 -0700

    Scaffolding to prepare for command-line arguments.

--- src/main.rs
+++ src/main.rs
@@ -6,23 +6,27 @@ mod renderer;
 mod test_lib;
 
 use crate::lib::*;
+use structopt::StructOpt;
+
+#[derive(StructOpt)]
+/// Renders a handlebars template from STDIN.
+#[structopt(after_help = "EXAMPLES:\n    echo \"{{#if true}}hello world{{/if}}\" | gluon")]
+struct CommandLineOptions {
+    // Options will go here.
+}
 
 const VERSION: GluonVersion = GluonVersion(env!("CARGO_PKG_VERSION"));
 
-const USAGE: &str = "Usage: gluon
-
-Renders a handlebars template from STDIN.
-
-Try: echo \"{{#if true}}hello world{{/if}}\" | gluon";
-
 fn main() {
+    let _options = CommandLineOptions::from_args();
+
     let stdin = io::stdin();
     let stdout = io::stdout();
     let mut input = stdin.lock();
     let mut output = stdout.lock();
 
     if let Err(err) = cli::render(VERSION, &mut input, &mut output).map_err(anyhow::Error::from) {
-        eprintln!("Error: {:?}\n\n{}", err, USAGE);
+        eprintln!("Error: {:?}", err);
         std::process::exit(1);
     }
 }
