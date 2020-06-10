aead594e14e9029e6d876e747ba487cc1fc0e946
Matt Kantor <the.matt.kantor@gmail.com>
2020-06-09 21:26:57 -0700

    Update CLI to use subcommands.

--- src/main.rs
+++ src/main.rs
@@ -9,27 +9,39 @@ use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(StructOpt)]
-/// Renders a handlebars template from STDIN.
-#[structopt(after_help = "EXAMPLES:\n    echo \"{{#if true}}hello world{{/if}}\" | gluon .")]
-struct CommandLineOptions {
-    #[structopt(parse(from_os_str))]
-    content_directory: PathBuf,
+enum GluonCommand {
+    /// Evaluates a handlebars template from STDIN.
+    #[structopt(
+        after_help = "EXAMPLES:\n    echo \"{{#if true}}hello world{{/if}}\" | gluon render --content-directory=."
+    )]
+    Render {
+        #[structopt(long, parse(from_os_str))]
+        content_directory: PathBuf,
+    },
 }
 
 const VERSION: GluonVersion = GluonVersion(env!("CARGO_PKG_VERSION"));
 
 fn main() {
-    let options = CommandLineOptions::from_args();
+    let result = match GluonCommand::from_args() {
+        GluonCommand::Render { content_directory } => {
+            let stdin = io::stdin();
+            let stdout = io::stdout();
+            let mut input = stdin.lock();
+            let mut output = stdout.lock();
 
-    let stdin = io::stdin();
-    let stdout = io::stdout();
-    let mut input = stdin.lock();
-    let mut output = stdout.lock();
+            cli::render(&content_directory, VERSION, &mut input, &mut output)
+                .map_err(anyhow::Error::from)
+        }
+    };
 
-    if let Err(err) = cli::render(&options.content_directory, VERSION, &mut input, &mut output)
-        .map_err(anyhow::Error::from)
-    {
-        eprintln!("Error: {:?}", err);
-        std::process::exit(1);
+    match result {
+        Err(error) => {
+            eprintln!("Error: {:?}", error);
+            std::process::exit(1);
+        }
+        Ok(_) => {
+            std::process::exit(0);
+        }
     }
 }
