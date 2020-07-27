eaa3cb0ad5f07243abf5bba4e5d7073bdce8578c
Matt Kantor <the.matt.kantor@gmail.com>
2020-07-27 15:06:53 -0700

    Implement index for HTTP server.

--- src/http.rs
+++ src/http.rs
@@ -4,44 +4,64 @@ use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use std::net::ToSocketAddrs;
 use std::sync::{Arc, RwLock};
 
-pub fn run_server<A, E>(content_engine: Arc<RwLock<E>>, index_address: &str, socket_address: A)
-where
+struct AppData<E: 'static + ContentEngine + Send + Sync> {
+    locked_content_engine: Arc<RwLock<E>>,
+    index_address: String,
+}
+
+pub fn run_server<A, E>(
+    locked_content_engine: Arc<RwLock<E>>,
+    index_address: &str,
+    socket_address: A,
+) where
     A: 'static + ToSocketAddrs,
     E: 'static + ContentEngine + Send + Sync,
 {
+    let index_address = String::from(index_address);
+
     log::info!("Initializing HTTP server");
     let mut system = System::new("server");
     system
         .block_on(async move {
             HttpServer::new(move || {
                 App::new()
-                    .app_data(content_engine.clone())
-                    .route("/{address:.*}", web::get().to(get::<E>))
+                    .app_data(AppData {
+                        locked_content_engine: locked_content_engine.clone(),
+                        index_address: index_address.clone(),
+                    })
+                    .route("/{path:.*}", web::get().to(get::<E>))
             })
             .bind(socket_address)?
             .run()
             .await
         })
         .unwrap(); // TODO: Return Result.
+
     log::info!("HTTP server has terminated");
 }
 
 async fn get<E: 'static + ContentEngine + Send + Sync>(request: HttpRequest) -> HttpResponse {
-    let address = request
+    let path = request
         .match_info()
-        .get("address") // TODO: Use index_address if address is empty.
-        .expect("No address provided!");
+        .get("path")
+        .expect("Failed to match request path!");
 
-    log::info!("Getting content for \"/{}\"", address);
+    log::info!("Getting content for \"/{}\"", path);
 
-    let locked_engine = request
-        .app_data::<Arc<RwLock<E>>>()
+    let app_data = request
+        .app_data::<AppData<E>>()
         .expect("App data was not of the expected type!");
 
-    let content_engine = locked_engine
+    let content_engine = app_data
+        .locked_content_engine
         .read()
         .expect("RwLock for ContentEngine has been poisoned");
 
+    let address = if path.is_empty() {
+        &app_data.index_address
+    } else {
+        path
+    };
     let result = content_engine.get(address).map(|content| {
         // TODO: Content negotation!
         let render_context = content_engine.get_render_context(&mime::TEXT_HTML);
@@ -52,11 +72,14 @@ async fn get<E: 'static + ContentEngine + Send + Sync>(request: HttpRequest) -> 
     // site with dynamic state. Maybe make these logs trace level?
     match result {
         Some(Ok(body)) => {
-            log::info!("Successfully rendered content for \"/{}\"", address);
+            log::info!(
+                "Successfully rendered content from address \"/{}\"",
+                address
+            );
             HttpResponse::Ok().body(body)
         }
         Some(Err(error)) => {
-            log::warn!("Failed to render content for \"/{}\"", address);
+            log::warn!("Failed to render content from address \"/{}\"", address);
             HttpResponse::InternalServerError().body(error.to_string())
         }
         None => {
