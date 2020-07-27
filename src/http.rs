2720db40ca288ed64a7ec360956acae1f10aa46b
Matt Kantor <the.matt.kantor@gmail.com>
2020-07-27 13:19:27 -0700

    ContentEngine + HTTP = 💖.

--- src/http.rs
+++ src/http.rs
@@ -1,7 +1,6 @@
 use crate::content::*;
 use actix_rt::System;
-use actix_web::{middleware, web, App, HttpResponse, HttpServer, Responder};
-use log::*;
+use actix_web::{web, App, HttpRequest, HttpResponse, HttpServer};
 use std::net::ToSocketAddrs;
 use std::sync::{Arc, RwLock};
 
@@ -10,24 +9,59 @@ where
     A: 'static + ToSocketAddrs,
     E: 'static + ContentEngine + Send + Sync,
 {
-    info!("Initializing HTTP server");
+    log::info!("Initializing HTTP server");
     let mut system = System::new("server");
     system
         .block_on(async move {
-            HttpServer::new(|| {
+            HttpServer::new(move || {
                 App::new()
-                    .wrap(middleware::Logger::default())
-                    .route("/", web::get().to(index))
+                    .app_data(content_engine.clone())
+                    .route("/{address:.*}", web::get().to(get::<E>))
             })
             .bind(socket_address)?
             .run()
             .await
         })
         .unwrap(); // TODO: Return Result.
-    info!("HTTP server has terminated");
+    log::info!("HTTP server has terminated");
 }
 
-async fn index() -> impl Responder {
-    debug!("👋 yo!");
-    HttpResponse::Ok().body("Hello world!")
+async fn get<E: 'static + ContentEngine + Send + Sync>(request: HttpRequest) -> HttpResponse {
+    let address = request
+        .match_info()
+        .get("address") // TODO: Use index_address if address is empty.
+        .expect("No address provided!");
+
+    log::info!("Getting content for \"/{}\"", address);
+
+    let locked_engine = request
+        .app_data::<Arc<RwLock<E>>>()
+        .expect("App data was not of the expected type!");
+
+    let content_engine = locked_engine
+        .read()
+        .expect("RwLock for ContentEngine has been poisoned");
+
+    let result = content_engine.get(address).map(|content| {
+        // TODO: Content negotation!
+        let render_context = content_engine.get_render_context(&mime::TEXT_HTML);
+        content.render(&render_context)
+    });
+
+    // FIXME: Is logging response status problematic? It could leak info on a
+    // site with dynamic state. Maybe make these logs trace level?
+    match result {
+        Some(Ok(body)) => {
+            log::info!("Successfully rendered content for \"/{}\"", address);
+            HttpResponse::Ok().body(body)
+        }
+        Some(Err(error)) => {
+            log::warn!("Failed to render content for \"/{}\"", address);
+            HttpResponse::InternalServerError().body(error.to_string())
+        }
+        None => {
+            log::warn!("No content found at \"/{}\"", address);
+            HttpResponse::NotFound().body("Not found.")
+        }
+    }
 }
