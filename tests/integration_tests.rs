8439bf6a69a237afbea47cdf32d9e175860303fc
Matt Kantor <the.matt.kantor@gmail.com>
2020-07-28 15:20:52 -0700

    Integration test for `render` subcommand.

--- tests/integration_tests.rs
+++ tests/integration_tests.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::ffi::OsStr;
-use std::process::Command;
+use std::io::Write;
+use std::process::{Command, Stdio};
 
 fn soliton_command<I, S>(args: I) -> Command
 where
@@ -45,6 +46,43 @@ fn invalid_subcommand_is_error() {
     assert!(
         !output.status.success(),
         "Executing `{:?}` succeeded when it should have failed",
+        command
+    );
+}
+
+#[test]
+fn render_subcommand_succeeds() {
+    let input = "{{#if true}}hello world{{/if}}";
+    let expected_output = "hello world";
+
+    let mut command = soliton_command(&[
+        "render",
+        "--content-directory=/dev/null",
+        "--source-media-type=text/plain",
+        "--target-media-type=text/plain",
+    ]);
+    command
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped());
+    let mut child = command.spawn().expect("Failed to spawn process");
+    child
+        .stdin
+        .as_mut()
+        .expect("Failed to capture child process stdin")
+        .write_all(input.as_bytes())
+        .expect("Failed to write to child process stdin");
+    let output = child.wait_with_output().expect("Failed to execute process");
+
+    assert!(
+        output.status.success(),
+        "Executing `{:?}` failed when it should have succeeded",
+        command
+    );
+    assert_eq!(
+        String::from_utf8(output.stdout).expect("Output was not valid UTF-8"),
+        expected_output,
+        "Executing `{:?}` did not produce the expected output",
         command
     );
 }
